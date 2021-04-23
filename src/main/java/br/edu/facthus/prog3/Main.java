4357acf9d5f5fa3025dc40f963b246eda09a0203
Fernando Mattioli <mattioli.fernando@gmail.com>
2021-04-22 21:00:45 -0300

    Commit inicial

---
+++ src/main/java/br/edu/facthus/prog3/Main.java
@@ -0,0 +1,43 @@
+package br.edu.facthus.prog3;
+
+public class Main {
+	
+	public static void exercicioGrupo01() {
+		// TODO: acrescentar nomes grupo 1
+	}
+
+	public static void exercicioGrupo02() {
+		// TODO: acrescentar nomes grupo 2
+	}
+
+	public static void exercicioGrupo03() {
+		// TODO: acrescentar nomes grupo 3
+	}
+
+	public static void exercicioGrupo04() {
+		// TODO: acrescentar nomes grupo 4
+	}
+
+	public static void exercicioGrupo05() {
+		// TODO: acrescentar nomes grupo 5
+	}
+
+	public static void exercicioGrupo06() {
+		// TODO: acrescentar nomes grupo 6
+	}
+
+	public static void exercicioGrupo07() {
+		// TODO: acrescentar nomes grupo 7
+	}
+
+	public static void main(String[] args) {
+		exercicioGrupo01();
+		exercicioGrupo02();
+		exercicioGrupo03();
+		exercicioGrupo04();
+		exercicioGrupo05();
+		exercicioGrupo06();
+		exercicioGrupo07();
+	}
+
+}
