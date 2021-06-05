d0d5d91b59c491703d62608ccdea91ea6b1dc6f4
Theninx <The.well@hotmail.com>
2021-06-05 18:06:08 -0300

    Atualiza Categoria

--- src/main/java/br/edu/facthus/os/service/CategoriasService.java
+++ src/main/java/br/edu/facthus/os/service/CategoriasService.java
@@ -1,6 +1,27 @@
 package br.edu.facthus.os.service;
 
-// T13: Atualizar
-public class CategoriasService {
+import java.util.List;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.edu.facthus.os.model.Categoria;
+import br.edu.facthus.os.model.Material;
+
+// T13: Atualizar
+@Stateless
+public class CategoriasService {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public void cadastraCategoria(Categoria categoria) {
+        entityManager.persist(categoria);
+    }
+
+    public List<Categoria> buscaTodos() {
+        return entityManager
+                .createNamedQuery("Categoria.findAll", Categoria.class)
+                .getResultList();
+    }
 }
