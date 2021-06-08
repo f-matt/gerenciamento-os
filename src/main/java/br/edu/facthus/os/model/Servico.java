70d057505b0a80a80af4d12ffc4a411aa93327f1
Mário Filho <Mrio Filho@PC>
2021-06-07 21:50:21 -0300

    Alteração T11

--- src/main/java/br/edu/facthus/os/model/Servico.java
+++ src/main/java/br/edu/facthus/os/model/Servico.java
@@ -1,10 +1,80 @@
 package br.edu.facthus.os.model;
 
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
 // T11: Atualizar
-public class Servico {
+
+@Entity
+@NamedQueries({
+	@NamedQuery(name = "Servico.findAll", 
+			query = "SELECT s "
+			+ "FROM Servico s "
+			+ "ORDER BY s.descricao")
+})
+public class Servico implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String descricao;
+	
+	/*
+	 * Auto-generated
+	 */
+	
+	@Override
+	public String toString() {
+		return "Servico [id=" + id + "]";
+	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Servico other = (Servico) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 }
