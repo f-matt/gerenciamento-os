65f55f87a09bbadfdf67a1184a4e3fcc14d34ed7
Mário Filho <Mrio Filho@PC>
2021-06-07 21:52:10 -0300

    Alteração T16

--- src/main/java/br/edu/facthus/os/service/ServicosService.java
+++ src/main/java/br/edu/facthus/os/service/ServicosService.java
@@ -1,6 +1,28 @@
 package br.edu.facthus.os.service;
 
-// T16: Atualizar
-public class ServicosService {
+import java.util.List;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.edu.facthus.os.model.Servico;
+
+// T16: Atualizar
+
+@Stateless
+public class ServicosService {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public void cadastraServicos(Servico servico) {
+		entityManager.persist(servico);
+	}
+	
+	public List<Servico> buscaTodos() {
+		return entityManager
+				.createNamedQuery("Servico.findAll", Servico.class)
+				.getResultList();
+	}
 }
