d90a3aa61f4f9fed116fbb799db1c9e2b1db1677
nogue <nogue@LAPTOP-MGCFD1OR>
2021-06-08 20:12:41 -0300

    Acrescenta a tela cliente

--- src/main/java/br/edu/facthus/os/service/ClientesService.java
+++ src/main/java/br/edu/facthus/os/service/ClientesService.java
@@ -1,6 +1,27 @@
 package br.edu.facthus.os.service;
 
-// T14: Atualizar
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.edu.facthus.os.model.Cliente;
+
+
 public class ClientesService {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public void cadastraCliente(Cliente cliente) {
+		entityManager.persist(cliente);
+	}
+	
+	public List<Cliente> buscaTodos() {
+		return entityManager
+				.createNamedQuery("Cliente.findAll", Cliente.class)
+				.getResultList();
+	}
+
 
 }
