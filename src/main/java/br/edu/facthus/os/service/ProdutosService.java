af0dd3a704e6f5aaecf5c4c51011b14ec72f0a55
csros <csros@DESKTOP-K2L78OP>
2021-06-16 08:12:09 -0300

    Ticketi T15- Grupo 01

--- src/main/java/br/edu/facthus/os/service/ProdutosService.java
+++ src/main/java/br/edu/facthus/os/service/ProdutosService.java
@@ -1,6 +1,32 @@
 package br.edu.facthus.os.service;
 
-// T15: Atualizar
-public class ProdutosService {
+import java.util.List;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.edu.facthus.os.model.Produto;
+
+// T15: Atualizar
+
+ @Stateless
+ 
+public class ProdutosService {
+@PersistenceContext
+
+	private EntityManager entityManager;
+
+	public void cadastraProdutos(Produto produto) {
+		entityManager.persist(produto);
+		
+		
+	}
+	public List<Produto> buscaTodos(){
+		return entityManager
+				.createNamedQuery("Produto.findAll", Produto.class)
+				.getResultList();
+	}
+	 
+	 
 }
